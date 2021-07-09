9ea61121ab848f09a09f12dbfec05daaede8f38d
Dando513 <49767042+Dando513@users.noreply.github.com>
2021-07-08 21:52:41 -0400

    fixed js to make line move across numbers

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const progress = document.getElementById('progress')
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
-const circles = document.getElementById('.circle')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
 
 let currentActive = 1
 
@@ -11,15 +11,17 @@ next.addEventListener('click', () => {
     if(currentActive > circles.length) {
         currentActive = circles.length
     }
+
     update()
 })
 
 prev.addEventListener('click', () => {
     currentActive--
-    
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-        }   
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
     update()
 })
 
@@ -27,21 +29,21 @@ function update() {
     circles.forEach((circle, idx) => {
         if(idx < currentActive) {
             circle.classList.add('active')
-        }  else  {
+        } else {
             circle.classList.remove('active')
         }
-})
-    const actives = document.querySelectorAll('active')
+    })
 
-    progress.style.width = (actives.length - 1) / (circle.length -1) * 100 + '%'
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
 
     if(currentActive === 1) {
         prev.disabled = true
-    } else if(currenActive === circles.length) {
+    } else if(currentActive === circles.length) {
         next.disabled = true
     } else {
         prev.disabled = false
         next.disabled = false
     }
-
 }
